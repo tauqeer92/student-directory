2e09d4197fa3a42ac33748321bcb48aa35451b3d
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-09-08 18:17:29 +0100

    Refactored

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-student_count = 11
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november},
-]
-
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
